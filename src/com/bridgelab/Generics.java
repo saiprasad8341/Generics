969c3bf2f2aafa62585c4f07c624d2240d256cea
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-11-14 18:51:13 +0530

    Welcome to the Generics

---
+++ src/com/bridgelab/Generics.java
@@ -0,0 +1,9 @@
+package com.bridgelab;
+
+import java.util.Scanner;
+
+public class Generics {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Generics of Maximum Problem");
+    }
+}
