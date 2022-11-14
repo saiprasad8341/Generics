659a3f2f6608df5d1fb7f6f2a8fa362ba1c3106e
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-11-14 18:53:06 +0530

    Finding Maximum of 3 Integers

--- src/com/bridgelab/Generics.java
+++ src/com/bridgelab/Generics.java
@@ -5,5 +5,21 @@ import java.util.Scanner;
 public class Generics {
     public static void main(String[] args) {
         System.out.println("Welcome to the Generics of Maximum Problem");
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a value :: ");
+        a = sc.nextInt();
+        System.out.print("Enter b value :: ");
+        b = sc.nextInt();
+        System.out.print("Enter c value :: ");
+        c = sc.nextInt();
+
+        if (a > b && a > c){
+            System.out.println(a+" is bigger");
+        } else if (b > a && b > c) {
+            System.out.println(b+ " is bigger");
+        }else {
+            System.out.println(c+ " is bigger");
+        }
     }
 }
